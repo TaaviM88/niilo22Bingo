4051a9b643157e76c76de301e3bd6b822836691d
Mutti88 <juomakuski@hotmail.com>
2020-04-21 01:14:49 +0300

    stuff added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var usedNums = new Array(76);
-
+var added_bingoelements =[];
 function newCard() {
 	//Starting loop through each square card
 	for(var i=0; i < 24; i++) {  //<--always this code for loops. change in red
@@ -10,14 +10,14 @@ function newCard() {
 function setSquare(thisSquare) {
 	var currSquare = "square"+thisSquare;
 	var newNum;
-	
+
 	var colPlace =new Array(0,1,2,3,4,0,1,2,3,4,0,1,3,4,0,1,2,3,4,0,1,2,3,4);
-	
+
 	do {
 		newNum =(colPlace[thisSquare] * 15) + getNewNum() + 1;
 	}
 	while (usedNums[newNum]);
-	
+
 	usedNums[newNum] = true;
   newNum = newNum.replace(/[0-9]/g,'');
 	document.getElementById(currSquare).innerHTML = newNum;
@@ -34,7 +34,7 @@ function getNewNum() {
 "lällätys-ääni(katsojien imitoiminen)",
 "liikunta boosaus",
 "ääniraidan vittuilu",
-"yhdellä kädellä taitelu ja sen tuskailu",
+"yhdellä kädellä taiteilu ja sen tuskailu",
 "random suuttuminen",
 "painovoiman vittuilu",
 "luetaan väärin",
@@ -48,18 +48,46 @@ function getNewNum() {
 "ongelmia laskemisen kanssa",
 "videon lopettaminen useaan otteeseen",
 "videolla sikailu",
-"lahjoituksena: bengalin tiiker/makaroni"];
+"lahjoituksena: bengalin tiikeri",
+"lahjoituksena: makaroni",
+"Syömistä videolla",
+"käsketään pistää ranttaliksi",
+"tekosyy, miksei asia x onnistu",
+"Niilo arvaa",
+];
 
-var randomItem = bingo_elements[Math.floor(Math.random()*bingo_elements.length)];
 
+var randomItem;// = bingo_elements[Math.floor(Math.random()*bingo_elements.length)];
+var b = false;
+do{
+  randomItem = bingo_elements[Math.floor(Math.random()*bingo_elements.length)];
+
+	if(added_bingoelements.includes(randomItem)===false)
+	{
+		added_bingoelements.push(randomItem);
+		b = true;
+	}
+
+}
+while(!b);
+
+
+ /*for(var i=0;i<added_bingoelements.length;i++)
+    {
+        if(added_bingoelements[i]===randomItem)
+        {
+
+        }
+    }*/
 	return randomItem; //Math.floor(Math.random() * 75);
-	
+
 }
 
 function anotherCard() {
-	/*for(var i=1; i<usedNums.length; i++) {
+	for(var i=1; i<usedNums.length; i++) {
 	usedNums[i] = false;
 	}
-	*/
+	added_bingoelements = [];
+	console.log(added_bingoelements);
 	newCard();
 }
